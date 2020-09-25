b93bd3d2c2efd84f8b5a530474418b4bcaa78cca
Eric07110904 <azsx26735546@gmail.com>
2020-09-26 04:21:45 +0800

    add setting View

---
+++ src/views/setting/components/Userprofile.vue
@@ -0,0 +1,38 @@
+<template>
+    <el-card style="width:100%" shadow="hover"> 
+        <div slot="header" class="clearfix">
+            <span>使用者資訊</span>
+        </div>
+    </el-card>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style>
+  .text {
+    font-size: 14px;
+  }
+
+  .item {
+    margin-bottom: 18px;
+  }
+
+  .clearfix:before,
+  .clearfix:after {
+    display: table;
+    content: "";
+  }
+  .clearfix:after {
+    clear: both
+  }
+
+  .box-card {
+    width: 480px;
+  }
+  .el-card__header{
+      background-color: #2286c3;
+      color: white;
+  }
+</style>
--- src/views/setting/index.vue
+++ src/views/setting/index.vue
@@ -1,12 +1,26 @@
 <template>
-  <div>
-    
+  <div class="app-container" style="height:100%">
+    <el-row :gutter="20" >
+      <el-col :span="8" :xs="24" style="margin-bottom:10px;">
+        <!-- Userprofile component -->
+        <Userprofile></Userprofile>
+      </el-col>
+
+      <el-col :span="16" :xs="24" style="margin-bottom:10px;">
+        <!-- Agency component -->
+        <Agency></Agency>
+      </el-col>
+    </el-row>
   </div>
 </template>
 
 
 <script>
+import Userprofile from './components/Userprofile'
+import Agency from './components/Agency'
 export default {
-    
+    components:{
+      Userprofile,Agency
+    }
 }
 </script>
